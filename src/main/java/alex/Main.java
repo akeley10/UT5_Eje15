7399a204cf1e1713ba9ebecdd18616ed8d48711c
akeley10 <91066025+akeley10@users.noreply.github.com>
2022-01-20 20:19:16 +0000

    Create Two Arrays

--- src/main/java/alex/Main.java
+++ src/main/java/alex/Main.java
@@ -3,5 +3,16 @@ package alex;
 public class Main {
     public static void main(String[] args) {
 
+        String[] days = new String[7];
+        days[0] = "Lunes";
+        days[1] = "Martes";
+        days[2] = "Miercoles";
+        days[3] = "Jueves";
+        days[4] = "Viernes";
+        days[5] = "Sabado";
+        days[6] = "Domingo";
+
+        String[] LireralDays =  {"Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo"};
+
     }
 }
